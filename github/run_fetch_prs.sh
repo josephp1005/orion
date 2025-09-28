08d2b05ebd80613594ab5e80cfcc75645c2197c8
Sahil Samar <sahilsamar031@gmail.com>
2025-09-28 05:17:37 -0400

    fix

--- github/run_fetch_prs.sh
+++ github/run_fetch_prs.sh
@@ -2,6 +2,6 @@
 
 # Script to execute 'fetch_prs.py' every 5 minutes
 while true; do
-    python3 fetch_prs.py josephp1005/orion
+    python3 fetch_prs.py josephp1005/ai-b2b-saas
     sleep 300  # Sleep for 5 minutes (300 seconds)
 done
